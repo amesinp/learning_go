a297660740f434dd33d5a5036f9c4bbf2dee4195
amesinp <amesinlolaprecious@gmail.com>
2021-01-03 23:37:20 +0100

    adds crash course code file

---
+++ traversy_crash/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/amesinp/learning_go/traversy_crash/strutil"
+)
+
+func greet(name string) string {
+	return "Hello, " + name
+}
+
+func printReverse(str string) {
+	fmt.Println(strutil.Reverse(str))
+}
+
+func main() {
+	var name = "Precious"
+	var age int32 = 21
+	const userType = "None"
+
+	// Shorthand syntax
+	email, isNew := "amesinlolaprecious@gmail.com", true
+
+	fmt.Println(name, age, userType, email, isNew)
+	fmt.Println(greet(name))
+
+	printReverse("hannah")
+
+	// Arrays
+
+	var fruitArr [2]string
+
+	fruitArr[0] = "Apple"
+	fruitArr[1] = "Orange"
+
+	// Declare and assign
+	colourArr := [2]string{"Red", "White"}
+
+	colourSlice := []string{"Red", "White", "Black", "Blue"}
+
+	fmt.Println(fruitArr)
+	fmt.Println(colourArr)
+	fmt.Println(colourSlice)
+
+	if age < 18 {
+		fmt.Println(name + " is a minor")
+	} else {
+		fmt.Println(name + " is an adult")
+	}
+
+	switch userType {
+	case "Admin":
+		fmt.Println("Welcome to the admin panel")
+	case "User":
+		fmt.Println("Welcome to the user panel")
+	default:
+		fmt.Println("Welcome to the default panel")
+	}
+
+	// Loop
+
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("Number %d\n", i)
+	}
+
+	// Long method
+	x := 1
+	for x <= 3 {
+		fmt.Println(x)
+		x++
+	}
+
+	// Maps
+
+	numbers := make(map[int]string)
+
+	numbers[1] = "One"
+	numbers[2] = "Two"
+
+	fmt.Println(numbers)
+
+	anotherNumbers := map[int]string{1: "One", 2: "Two", 3: "Three"}
+
+	fmt.Println(anotherNumbers)
+	delete(anotherNumbers, 1)
+	fmt.Println(anotherNumbers)
+
+}
